ca4f3fa1cfef39dc11313ca5d566b6009c5513fa
Mykola Repetskyi <22MRQA@gmail.com>
2025-08-25 13:08:51 +0300

    Add завдання 5

--- src/test/java/homePageTests/HomePageTest.java
+++ src/test/java/homePageTests/HomePageTest.java
@@ -1,14 +1,14 @@
 package homePageTests;
 
 
-import com.beust.ah.A;
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
+//import com.beust.ah.A;
+//import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
+//import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import static java.lang.Thread.sleep;
