c5b6097aaf97dc9eaa453a1b6b29efd2b53960ce
Mykola Repetskyi <22MRQA@gmail.com>
2025-08-25 16:04:41 +0300

    delete 4 5 6

--- src/test/java/homePageTests/HomePageTest.java
+++ src/test/java/homePageTests/HomePageTest.java
@@ -3,6 +3,7 @@ package homePageTests;
 
 //import com.beust.ah.A;
 //import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -63,7 +64,7 @@ public class HomePageTest {
 //    3 Завдання
 
     @Test
-        public void checkLogoAllo () throws InterruptedException {
+    public void checkLogoAllo() throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
 
@@ -117,75 +118,5 @@ public class HomePageTest {
     }
 
 //    4 Завдання
-
-    @Test
-    public void checkHeaderItems () throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-
-        driver.get("https://allo.ua/");
-
-        WebElement buttonPokupcyam = driver.findElement(By.xpath("//div[@class='mh-button__wrap']"));
-
-        Assert.assertTrue(buttonPokupcyam.isDisplayed());
-
-        buttonPokupcyam.click();
-
-        sleep(2000);
-
-        WebElement dropDownMenu = driver.findElement(By.xpath("//div[@class='mh-button__dropdown']"));
-
-        Assert.assertTrue(dropDownMenu.isDisplayed());
-
-        WebElement checkButoonOplataDostavkaDislayd = driver.findElement(By.xpath("(//a[@class='mh-button'])[5]"));
-
-        Assert.assertTrue(checkButoonOplataDostavkaDislayd.isDisplayed());
-
-        checkButoonOplataDostavkaDislayd.click();
-
-        WebElement checkTitleDostavkaOplata = driver.findElement(By.xpath("//h2[@class='sp-page-title sp-h2 page-header']"));
-
-        String checkTitleTextOplataDostavka  = checkTitleDostavkaOplata.getText();
-
-        Assert.assertTrue(checkTitleTextOplataDostavka.contains("Доставка і оплата"));
-
-        WebElement displaydYakOformutuZmovlenya = driver.findElement(By.xpath("(//h3[@class='sub-block-header'])[1]"));
-
-        Assert.assertTrue(displaydYakOformutuZmovlenya.isDisplayed());
-
-        String checkDisplaydYakOformutuZmovlenya = displaydYakOformutuZmovlenya.getText();
-
-        Assert.assertTrue(checkDisplaydYakOformutuZmovlenya.contains("Як оформити замовлення?"));
-
-        System.out.println(checkDisplaydYakOformutuZmovlenya);
-
-
-//        driver.quit();
-
-    }
-
-    // Завдання 5
-    @Test
-    public void openWindiwMoyo () {
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-
-        driver.get("https://www.moyo.ua/");
-
-
-    }
-// завдання 6
-
-    @Test
-    public void openWindiwMiezu(){
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().window().maximize();
-
-        driver.get("https://www.meizu.com/global");
-    }
-
 
 }
